21f108076f39b6884b0ac30b09034c42b51bde37
liuyi <yi7.liu@symbio.com>
2023-01-29 14:47:23 +0800

    组件排序

--- src/components/layout/index.jsx
+++ src/components/layout/index.jsx
@@ -1,33 +1,35 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef, useCallback } from "react";
 import { useNavigate } from "react-router-dom";
 import { comps } from "../../data";
 import { Comp } from "../comp";
+import down from "../../assets/down.svg";
+import up from "../../assets/up.svg";
 import "./index.css";
 
 const Layout = () => {
   const [items, setItems] = useState([]);
   const [source, setSource] = useState();
   const [target, setTarget] = useState();
-  const [position, setPosition] = useState(-1);
+  const position = useRef(-1);
   const [selected, setSelected] = useState(-1);
   const navigate = useNavigate();
 
-  const onDragStart = (comp) => (e) => {
+  const onDragStart = (comp) => () => {
     setSource(comp);
   };
 
-  const onDragEnd = (comp) => (e) => {
+  const onDragEnd = () => () => {
     setSource(null);
     setTarget(null);
   };
 
   const onDrop = (e) => {
     e.preventDefault();
-    if (position >= 0) {
-      let newItems = [...items];
-      newItems.splice(position, 0, JSON.parse(JSON.stringify(target)));
+    if (position.current >= 0) {
+      const newItems = [...items];
+      newItems.splice(position.current, 0, JSON.parse(JSON.stringify(target)));
       setItems(newItems);
-      setSelected(position);
+      setSelected(position.current);
     } else {
       setItems([...items, JSON.parse(JSON.stringify(target))]);
       setSelected(items.length);
@@ -36,18 +38,56 @@ const Layout = () => {
 
   const onDragOver = (e) => {
     e.preventDefault();
-    e.target.getAttribute("data-id")
-      ? setPosition(e.target.getAttribute("data-id"))
-      : setPosition(-1);
+    if (isNaN(parseInt(e.target.getAttribute("data-id")))) {
+      position.current = -1;
+    } else {
+      position.current = parseInt(e.target.getAttribute("data-id"));
+    }
+
     setTarget(source);
   };
 
-  const onDragLeave = (e) => {
+  const onDragLeave = () => {
     setTarget(null);
+    position.current = -1;
   };
 
-  const onSelected = (id) => (e) => {
+  const onSelected = (id) => () => {
     setSelected(id);
+  };
+
+  const handleDown = (index) => (e) => {
+    e.stopPropagation();
+    if (index === items.length - 1) return;
+    const newItems = [...items];
+    const item = newItems[index];
+    newItems.splice(index, 1);
+    newItems.splice(index + 1, 0, item);
+    setItems(newItems);
+    setSelected(index + 1);
+  };
+
+  const handleUp = (index) => (e) => {
+    e.stopPropagation();
+    if (index === 0) return;
+    const newItems = [...items];
+    const item = newItems[index];
+    newItems.splice(index, 1);
+    newItems.splice(index - 1, 0, item);
+    setItems(newItems);
+    setSelected(index - 1);
+  };
+
+  const handleDelete = (index) => (e) => {
+    e.stopPropagation();
+    const newItems = [...items];
+    newItems.splice(index, 1);
+    if(newItems.length && index===0){
+      setSelected(0)
+    }else{
+      setSelected(index - 1);
+    }
+    setItems(newItems);
   };
 
   const onSave = () => {
@@ -59,9 +99,19 @@ const Layout = () => {
     navigate("/preview");
   };
 
+
   useEffect(() => {
-    window.localStorage.removeItem("comps");
+    const rawData = window.localStorage.getItem("comps");
+    if(rawData){
+      try {
+        setItems(JSON.parse(rawData));
+      } catch (e) {
+        console.warn("数据解析失败");
+      }
+    }
+   
   }, []);
+
 
   return (
     <div className="layout">
@@ -92,32 +142,50 @@ const Layout = () => {
               key={index}
               onClick={onSelected(index)}
               data-id={index}
-              className={`box ${selected == index ? "active" : ""}`}
+              className={`box ${selected === index ? "active" : ""}`}
             >
+              <div className="action">
+                <div
+                  className={`btn ${index === 0 ? "disabled" : ""}`}
+                  onClick={handleUp(index)}
+                >
+                  <img src={up} />
+                </div>
+                <div
+                  className={`btn ${
+                    index === items.length - 1 ? "disabled" : ""
+                  }`}
+                  onClick={handleDown(index)}
+                >
+                  <img src={down} />
+                </div>
+                <div className="btn" onClick={handleDelete(index)}>
+                  删除
+                </div>
+              </div>
               <Comp data={item} />
             </div>
           );
         })}
       </div>
       <div className="right">
-        {selected >= 0 &&
-          items[selected].props.map((prop, index) => {
-            return (
-              <div key={index}>
-                {prop.label}
-                <input
-                  key={Math.random()}
-                  defaultValue={prop.value}
-                  onBlur={() => {
-                    setItems([...items]);
-                  }}
-                  onChange={(e) => {
-                    prop.value = e.target.value;
-                  }}
-                />
-              </div>
-            );
-          })}
+        {selected>=0 && items[selected].props.map((prop, index) => {
+      return (
+        <div key={index}>
+          {prop.label}
+          <input
+            key={Math.random()}
+            defaultValue={prop.value}
+            onBlur={() => {
+              setItems([...items]);
+            }}
+            onChange={(e) => {
+              prop.value = e.target.value;
+            }}
+          />
+        </div>
+      );
+    })}
         <br />
         <button onClick={onSave}>保存</button>{" "}
         <button onClick={onPreview}>预览</button>
--- src/components/preview/index.jsx
+++ src/components/preview/index.jsx
@@ -1,24 +1,32 @@
-import { useEffect,useState } from "react"
-import { Comp } from "../comp"
-import './index.css'
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Comp } from "../comp";
+import "./index.css";
 
-const Preview=()=>{
-    const [data,setData]=useState([])
-    useEffect(()=>{
-      const rawData=window.localStorage.getItem('comps') || {}
-      try{
-        setData(JSON.parse(rawData))
-      }catch(e){
-        console.warn('数据解析失败')
+const Preview = () => {
+  const [items, setItems] = useState([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const rawData = window.localStorage.getItem("comps");
+    if(rawData){
+      try {
+        setItems(JSON.parse(rawData));
+      } catch (e) {
+        console.warn("数据解析失败");
       }
-    },[])
-    return <div className='preview'>
-       {data.map((item, index) => {
-          return (
-            <Comp data={item} key={index} />
-          );
-        })}
-    </div>
-}
+    }
+  }, []);
 
-export default Preview
+  return (
+    <div className="preview">
+      {items.map((item, index) => {
+        return <Comp data={item} key={index} />;
+      })}
+
+      <button onClick={() => navigate("/")}>返回</button>
+    </div>
+  );
+};
+
+export default Preview;
